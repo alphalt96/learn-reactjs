c1726d502360e37a3d88e9f44f12b2bb869235c6
anh <ngoquocanh22@gmail.com>
2023-02-16 16:21:57 +0700

    update: update animation for login button, create dropdown list with animation for sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,8 +26,39 @@ module.exports = {
         240: '240px'
       }
     },
+    keyframes: {
+      slideDown: {
+        '0%': {
+          transform: 'scaleY(0%)'
+        },
+        '80%': {
+          transform: 'scaleY(110%)'
+        },
+        '100%': {
+          transform: 'scaleY(100%)'
+        }
+      },
+      scrollUp: {
+        '0%': {
+          transform: 'scaleY(100%)'
+        },
+        '100%': {
+          transform: 'scaleY(0%)'
+        }
+      },
+      rotate180: {
+        '0%': {
+          transform: 'rotate(0deg)'
+        },
+        '100%': {
+          transform: 'rotate(180deg)'
+        }
+      }
+    },
     animation: {
-      fade: 'fadeOut 3s ease-in-out',
+      'slide-down': 'slideDown 200ms ease-in-out',
+      'rotate-180': 'rotate180 350ms ease-out both',
+      'scroll-up': 'scrollUp 350ms ease-out both'
     }
   },
   plugins: [],
